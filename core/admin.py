88c80d0133e3a5f86f25d0a148d6bafbf16e0c5c
mikepodolskiy <mikepodolskiy@gmail.com>
2023-07-05 20:21:32 +0300

    all transferred from HW33

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+from core.models import User
+
+class MyAdmin(admin.ModelAdmin):
+    list_filter = ("is_staff", "is_active", "is_superuser")
+    list_display = ("username", "email", "first_name", "last_name")
+    search_fields = ("username", "first_name", "last_name")
+    # exclude = ["password"]
+    fields = ("username", "first_name", "last_name", "email", "is_staff", "is_active", "last_login", "date_joined")
+    readonly_fields = ("last_login", "date_joined")
+
+
+admin.site.register(User, MyAdmin)
+admin.site.unregister(Group)
