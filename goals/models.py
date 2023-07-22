b764a66d547ad8773a5b20dc6a3532bc1dc408dc
mikepodolskiy <mikepodolskiy@gmail.com>
2023-07-22 10:58:45 +0300

    36 done another migrations without inheritance in models

--- goals/models.py
+++ goals/models.py
@@ -6,9 +6,12 @@ from django.utils import timezone
 from core.models import User
 
 
-class GoalsModelMixin(models.Model):
+class GoalCategory(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=255)
+    user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
+    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
     created = models.DateTimeField(verbose_name="Дата создания", blank=True, null=True)
-    updated = models.DateTimeField(verbose_name="Дата последнего обновления", blank=True, null = True)
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления", blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if not self.id:  # Когда объект только создается, у него еще нет id
@@ -16,18 +19,12 @@ class GoalsModelMixin(models.Model):
         self.updated = timezone.now()  # проставляем дату обновления
         return super().save(*args, **kwargs)
 
-
-class GoalCategory(GoalsModelMixin):
-    title = models.CharField(verbose_name="Название", max_length=255)
-    user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
-    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
-
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
 
-class Goal(GoalsModelMixin):
+class Goal(models.Model):
     class Status(models.IntegerChoices):
         to_do = 1, "К выполнению"
         in_progress = 2, "В процессе"
@@ -55,16 +52,32 @@ class Goal(GoalsModelMixin):
         default=Priority.medium
     )
     due_date = models.DateField(verbose_name='Дедлайн', null=True, blank=True)
+    created = models.DateTimeField(verbose_name="Дата создания", blank=True, null=True)
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления", blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:  # Когда объект только создается, у него еще нет id
+            self.created = timezone.now()  # проставляем дату создания
+        self.updated = timezone.now()  # проставляем дату обновления
+        return super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Цель"
         verbose_name_plural = "Цели"
 
 
-class Comment(GoalsModelMixin):
+class Comment(models.Model):
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
     text = models.CharField(verbose_name="Текст")
     goal = models.ForeignKey(Goal, verbose_name="Цель", on_delete=models.CASCADE)
+    created = models.DateTimeField(verbose_name="Дата создания", blank=True, null=True)
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления", blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:  # Когда объект только создается, у него еще нет id
+            self.created = timezone.now()  # проставляем дату создания
+        self.updated = timezone.now()  # проставляем дату обновления
+        return super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Комментарий"
